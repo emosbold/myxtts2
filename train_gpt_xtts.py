a57171e1d31dcded52817ec6dc6434634ad5a2e6
emosbold <emosbold@gmail.com>
2025-12-16 10:12:15 +0330

    Update train_gpt_xtts.py

--- train_gpt_xtts.py
+++ train_gpt_xtts.py
@@ -3,7 +3,7 @@ import gc
 
 from trainer import Trainer, TrainerArgs
 
-from TTS.config.shared_configs import BaseDatasetConfig, , CharactersConfig
+from TTS.config.shared_configs import BaseDatasetConfig, CharactersConfig
 from TTS.tts.datasets import load_tts_samples
 from TTS.tts.layers.xtts.trainer.gpt_trainer import GPTArgs, GPTTrainer, GPTTrainerConfig, XttsAudioConfig
 from TTS.utils.manage import ModelManager
